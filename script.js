0bca9d51a8a00a4af4343a85a22ca3649f94e625
Mah-noor312 <mahanoorwaheed45429@gmail.com>
2025-08-06 06:56:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  alert(`Thank you for your message, ${name}!`);
+});
